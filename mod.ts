22bed5d95af311bf4f78341416c025d2ac670dc7
daemon1024 <barun1024@gmail.com>
2021-01-26 20:35:23 +0530

    :art: makes boxes according to input content

--- mod.ts
+++ mod.ts
@@ -1,1 +1,7 @@
-console.log("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510\n\u2502 hello world \u2502\n\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+function bokusen(content:string) {
+    const text = "\u250c" + "\u2500".repeat(content.length + 2) + "\u2510\n\u2502 "  + content + " \u2502\n\u2514" + "\u2500".repeat(content.length + 2) + "\u2518";
+    console.log(text);
+}
+// console.log("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510\n\u2502 hello world \u2502\n\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+
+bokusen("hello world")
